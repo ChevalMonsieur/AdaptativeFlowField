5b54cdbf0b496685298d2c49c8894b828ae73bfd
Justin Hurson <just.1et2mii@gmail.com>
2023-10-06 13:12:44 +0200

    ajout 2 fonctionnalites

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ let opacity = 20; // how visible is the point (0 = invisible, 255 = opaque)
 let fadeSpeed = 0; // how fast the points will fade out (0 = no fade, 255 = instant fade)
 let borderRule = "randomTeleport"; // determines what happens when a point goes off-screen (randomTeleport, linkedTeleport, none)
 
-let mainWidth;
-let mainHeight;
-
+let mainWidth, mainHeight; // size of the canvas
+let currentColor; // current color of the points
 
 function setup() {
+
     // get size of div
     if (window.innerWidth > 1920) {
         mainWidth = document.getElementById("main").clientWidth;
@@ -26,7 +26,8 @@ function setup() {
     // initialize canvas
     createCanvas(mainWidth, mainHeight); // set canva size
     background(0); // set background color in A
-    stroke(0, 255, 200, opacity); // set color in RGBA
+    currentColor = color(0,255,200, opacity); // set base color in RGBA
+    stroke(currentColor); // set color in RGBA
     strokeWeight(size); // set size of points
 
     // create all points
@@ -37,7 +38,7 @@ function setup() {
 
 function draw() {
     if (fadeSpeed > 0) {
-        background(0, 0, 0, fadeSpeed);
+        background(0, fadeSpeed);
     }
     for (vector of points) {
         // set the direction of the point with semi-random noise
@@ -76,7 +77,7 @@ function windowResized() {
         // get new div size
         if (window.innerWidth > 1920) {
             mainWidth = document.getElementById("main").clientWidth;
-            mainHeight = innerHeight - 300; // test
+            mainHeight = innerHeight - 250; // make the canva take most screen space
         } else {
             mainWidth = document.getElementById("main").clientWidth;
             mainHeight = document.getElementById("setterCanvaHeight").clientHeight + 25; // + 25 is a manual offset to make canvas slightly bigger than the sidebar
@@ -91,6 +92,7 @@ function windowResized() {
 
 function reset() {
     //reset values
+    stroke(currentColor);
     background(0);
     totalOffset = 0;
 
@@ -122,14 +124,15 @@ function inputNbParticle(event) {
 }
 
 function inputSpeed(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
         speed = document.getElementById("speed").value; // change speed
         reset();
     }
 }
 
 function inputSize(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
+        console.log("inputSize");
         size = document.getElementById("size").value; // change size
         strokeWeight(size); // set size of points
         reset();
@@ -137,30 +140,47 @@ function inputSize(event) {
 }
 
 function inputPositionScale(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
+        console.log("inputPositionScale");
         positionScale = document.getElementById("positionScale").value; // change positionScale
         reset();
     }
 }
 
 function inputRotationScale(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
+        console.log("inputRotationScale");
         rotationScale = document.getElementById("rotationScale").value; // change rotationScale
         reset();
     }
 }
 
 function inputOpacity(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
+        console.log("inputOpacity");
         opacity = Math.floor(document.getElementById("opacity").value); // change opacity (USE MATH.FLOOR BECAUSE STROKE DOESN'T ACCEPT FLOATS)
-        stroke(0, 255, 200, opacity); // set color in RGBA
+        currentColor.setAlpha(opacity); // set opacity in color
+        stroke(currentColor); // set color in RGBA
         reset();
     }
 }
 
 function inputFade(event) {
-    if (event.keyCode == 13) { // check if enter is pressed
+    if (event.keyCode === 13) { // check if enter is pressed
+        console.log("inputFade");
         fadeSpeed = Math.floor(document.getElementById("fadeSpeed").value); // change fadeSpeed (USE MATH.FLOOR BECAUSE STROKE DOESN'T ACCEPT FLOATS)
         reset();
     }
+}
+
+function applyAllInputs() {
+    console.log("applyAllInputs");
+    // calls all inputs with correct event.keycode
+    inputNbParticle({keyCode: 13});
+    inputSpeed({keyCode: 13});
+    inputSize({keyCode: 13});
+    inputPositionScale({keyCode: 13});
+    inputRotationScale({keyCode: 13});
+    inputOpacity({keyCode: 13});
+    inputFade({keyCode: 13});
 }
